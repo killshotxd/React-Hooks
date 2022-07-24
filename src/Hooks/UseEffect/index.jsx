8fa957828dcec5edc3849660ad8573ebc656094c
killshotxd <hassanansari211@gmail.com>
2022-07-24 10:10:33 +0530

    fetching data from api using use effect

--- src/Hooks/UseEffect/index.jsx
+++ src/Hooks/UseEffect/index.jsx
@@ -9,6 +9,22 @@ function UseEffectHooks() {
   useEffect(() => {
     console.log("Use effect in Action but only once");
   }, []);
+
+  //fetch data from placeholder api on page Load
+
+  useEffect(() => {
+    async function getDataOnPageLoad() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+
+      const data = await response.json();
+      console.log(data);
+    }
+
+    getDataOnPageLoad();
+  }, []);
+
   return (
     <Fragment>
       <button onClick={() => setCounter(counter + 1)}>Click Me </button>
