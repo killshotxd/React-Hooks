aeaece00fa0d72dcd09b3e515c71b91bf5271348
killshotxd <hassanansari211@gmail.com>
2022-07-24 09:47:55 +0530

    handling multiple states

--- src/Hooks/usestate/index.jsx
+++ src/Hooks/usestate/index.jsx
@@ -5,15 +5,46 @@ import React, { Fragment, useState } from "react";
 
 const UseStateHook = () => {
   //state item, method, initial value
-  let [counter, setCounter] = useState(0); // initial value for counter
-  const handleClick = () => {
-    setCounter(counter + 1);
+  //   let [counter, setCounter] = useState(0); // initial value for counter
+  //   const handleClick = () => {
+  //     setCounter(counter + 1);
+  //   };
+
+  const initialState = {
+    name1: "",
+    name2: "",
   };
+
+  const [infoData, setInfoData] = useState(initialState);
+
+  const handleOnchange = (event) => {
+    const { name, value } = event.target;
+
+    setInfoData((prevState) => ({
+      ...prevState, //copy all the states
+      [name]: value, //updating state with new value
+    }));
+  };
+
+  console.log(infoData);
 
   return (
     <Fragment>
-      <button onClick={handleClick}>Counter</button>
-      <h3>{counter}</h3>
+      {/* <button onClick={handleClick}>Counter</button>
+      <h3>{counter}</h3> */}
+
+      <input
+        placeholder="Input One"
+        value={infoData.name1}
+        name="name1"
+        onChange={handleOnchange}
+      />
+      <input
+        placeholder="Input Two"
+        value={infoData.name2}
+        name="name2"
+        onChange={handleOnchange}
+      />
     </Fragment>
   );
 };
